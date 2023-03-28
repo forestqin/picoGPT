fc90728486d122f9dfbb4348cec2ec1ad913e78f
qintao361 <qintao361@126.com>
2023-03-28 14:51:29 +0800

    add install introduction

--- gpt2.py
+++ gpt2.py
@@ -13,7 +13,8 @@ def softmax(x):
 def layer_norm(x, g, b, eps: float = 1e-5):
     mean = np.mean(x, axis=-1, keepdims=True)
     variance = np.var(x, axis=-1, keepdims=True)
-    x = (x - mean) / np.sqrt(variance + eps)  # normalize x to have mean=0 and var=1 over last axis
+    x = (x - mean) / np.sqrt(
+        variance + eps)  # normalize x to have mean=0 and var=1 over last axis
     return g * x + b  # scale and offset with gamma/beta params
 
 
@@ -31,7 +32,9 @@ def ffn(x, c_fc, c_proj):  # [n_seq, n_embd] -> [n_seq, n_embd]
     return x
 
 
-def attention(q, k, v, mask):  # [n_q, d_k], [n_k, d_k], [n_k, d_v], [n_q, n_k] -> [n_q, d_v]
+def attention(
+        q, k, v,
+        mask):  # [n_q, d_k], [n_k, d_k], [n_k, d_v], [n_q, n_k] -> [n_q, d_v]
     return softmax(q @ k.T / np.sqrt(q.shape[-1]) + mask) @ v
 
 
@@ -43,16 +46,22 @@ def mha(x, c_attn, c_proj, n_head):  # [n_seq, n_embd] -> [n_seq, n_embd]
     qkv = np.split(x, 3, axis=-1)  # [n_seq, 3*n_embd] -> [3, n_seq, n_embd]
 
     # split into heads
-    qkv_heads = list(map(lambda x: np.split(x, n_head, axis=-1), qkv))  # [3, n_seq, n_embd] -> [3, n_head, n_seq, n_embd/n_head]
+    qkv_heads = list(
+        map(lambda x: np.split(x, n_head, axis=-1),
+            qkv))  # [3, n_seq, n_embd] -> [3, n_head, n_seq, n_embd/n_head]
 
     # causal mask to hide future inputs from being attended to
-    causal_mask = (1 - np.tri(x.shape[0], dtype=x.dtype)) * -1e10  # [n_seq, n_seq]
+    causal_mask = (1 -
+                   np.tri(x.shape[0], dtype=x.dtype)) * -1e10  # [n_seq, n_seq]
 
     # perform attention over each head
-    out_heads = [attention(q, k, v, causal_mask) for q, k, v in zip(*qkv_heads)]  # [3, n_head, n_seq, n_embd/n_head] -> [n_head, n_seq, n_embd/n_head]
+    out_heads = [
+        attention(q, k, v, causal_mask) for q, k, v in zip(*qkv_heads)
+    ]  # [3, n_head, n_seq, n_embd/n_head] -> [n_head, n_seq, n_embd/n_head]
 
     # merge heads
-    x = np.hstack(out_heads)  # [n_head, n_seq, n_embd/n_head] -> [n_seq, n_embd]
+    x = np.hstack(
+        out_heads)  # [n_head, n_seq, n_embd/n_head] -> [n_seq, n_embd]
 
     # out projection
     x = linear(x, **c_proj)  # [n_seq, n_embd] -> [n_seq, n_embd]
@@ -60,23 +69,28 @@ def mha(x, c_attn, c_proj, n_head):  # [n_seq, n_embd] -> [n_seq, n_embd]
     return x
 
 
-def transformer_block(x, mlp, attn, ln_1, ln_2, n_head):  # [n_seq, n_embd] -> [n_seq, n_embd]
+def transformer_block(x, mlp, attn, ln_1, ln_2,
+                      n_head):  # [n_seq, n_embd] -> [n_seq, n_embd]
     # multi-head causal self attention
-    x = x + mha(layer_norm(x, **ln_1), **attn, n_head=n_head)  # [n_seq, n_embd] -> [n_seq, n_embd]
+    x = x + mha(layer_norm(x, **ln_1), **attn,
+                n_head=n_head)  # [n_seq, n_embd] -> [n_seq, n_embd]
 
     # position-wise feed forward network
-    x = x + ffn(layer_norm(x, **ln_2), **mlp)  # [n_seq, n_embd] -> [n_seq, n_embd]
+    x = x + ffn(layer_norm(x, **ln_2), **
+                mlp)  # [n_seq, n_embd] -> [n_seq, n_embd]
 
     return x
 
 
-def gpt2(inputs, wte, wpe, blocks, ln_f, n_head):  # [n_seq] -> [n_seq, n_vocab]
+def gpt2(inputs, wte, wpe, blocks, ln_f,
+         n_head):  # [n_seq] -> [n_seq, n_vocab]
     # token + positional embeddings
     x = wte[inputs] + wpe[range(len(inputs))]  # [n_seq] -> [n_seq, n_embd]
 
     # forward pass through n_layer transformer blocks
     for block in blocks:
-        x = transformer_block(x, **block, n_head=n_head)  # [n_seq, n_embd] -> [n_seq, n_embd]
+        x = transformer_block(
+            x, **block, n_head=n_head)  # [n_seq, n_embd] -> [n_seq, n_embd]
 
     # projection to vocab
     x = layer_norm(x, **ln_f)  # [n_seq, n_embd] -> [n_seq, n_embd]
@@ -86,19 +100,25 @@ def gpt2(inputs, wte, wpe, blocks, ln_f, n_head):  # [n_seq] -> [n_seq, n_vocab]
 def generate(inputs, params, n_head, n_tokens_to_generate):
     from tqdm import tqdm
 
-    for _ in tqdm(range(n_tokens_to_generate), "generating"):  # auto-regressive decode loop
+    for _ in tqdm(range(n_tokens_to_generate),
+                  "generating"):  # auto-regressive decode loop
         logits = gpt2(inputs, **params, n_head=n_head)  # model forward pass
         next_id = np.argmax(logits[-1])  # greedy sampling
         inputs.append(int(next_id))  # append prediction to input
 
-    return inputs[len(inputs) - n_tokens_to_generate :]  # only return generated ids
+    return inputs[len(inputs) -
+                  n_tokens_to_generate:]  # only return generated ids
 
 
-def main(prompt: str, n_tokens_to_generate: int = 40, model_size: str = "124M", models_dir: str = "models"):
+def main(prompt: str,
+         n_tokens_to_generate: int = 40,
+         model_size: str = "124M",
+         models_dir: str = "models"):
     from utils import load_encoder_hparams_and_params
 
     # load encoder, hparams, and params from the released open-ai gpt-2 files
-    encoder, hparams, params = load_encoder_hparams_and_params(model_size, models_dir)
+    encoder, hparams, params = load_encoder_hparams_and_params(
+        model_size, models_dir)
 
     # encode the input string using the BPE tokenizer
     input_ids = encoder.encode(prompt)
@@ -107,7 +127,8 @@ def main(prompt: str, n_tokens_to_generate: int = 40, model_size: str = "124M", 
     assert len(input_ids) + n_tokens_to_generate < hparams["n_ctx"]
 
     # generate output ids
-    output_ids = generate(input_ids, params, hparams["n_head"], n_tokens_to_generate)
+    output_ids = generate(input_ids, params, hparams["n_head"],
+                          n_tokens_to_generate)
 
     # decode the ids back into a string
     output_text = encoder.decode(output_ids)
@@ -116,6 +137,7 @@ def main(prompt: str, n_tokens_to_generate: int = 40, model_size: str = "124M", 
 
 
 if __name__ == "__main__":
-    import fire
-
-    fire.Fire(main)
+    # import fire
+    # fire.Fire(main)
+    output_text = main("I like to eat apple.", model_size="355M")
+    print(f"{output_text=}")
